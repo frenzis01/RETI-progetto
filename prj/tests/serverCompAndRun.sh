d07c13f411e811edfb0b3f5918023ff121fea20d
frenzis01 <francesco.lorenzoni12042@gmail.com>
2022-01-28 00:34:26 +0100

    turned in this version

--- prj/tests/serverCompAndRun.sh
+++ prj/tests/serverCompAndRun.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BKPDIR="$SCRIPT_DIR/../bkp"
 cd "$SCRIPT_DIR"
+echo "{
+\"tcpPort\" : \"12345\",
+\"multicastPort\" : \"6789\",
+\"registryPort\" : \"1900\",
+\"authorPercentage\" : \"0.6\",
+\"backupDir\" : \"$BKPDIR\",
+\"exclusiveLogin\" : true
+}" > ../config/serverConfig.json
+
 javac -cp "../lib/*:../src:../out" -d "../out/" ../src/*.java ../src/exceptions/*.java
 java -cp "../lib/*:../out" ServerMain
